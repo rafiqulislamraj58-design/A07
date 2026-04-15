153efdfc0078b0e0a768a0c197ff5dd162ec845f
rafiqulislamraj58-design <rafiqulislamraj58@gmail.com>
2026-04-15 21:44:06 +0600

    home setion Reation section

---
+++ src/Componats/Relation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Relation = () => {
+  return (
+    <>
+        <div className='shadow bg-gray-100'>
+            <div className='flex justify- justify-between items-center p-3.5'>
+                <h1 className='text-[#244d3f] font-bold'>Relationship Goal</h1>
+                <button className='flex justify-center items-center bg-gray-300 p-1.5 rounded-md'>Edit</button>
+            </div>
+            <p className='p-3.5'>Connect every 30 days</p>
+        </div>
+    </>
+  )
+}
+
+export default Relation
--- src/pages/FrindDetalis.jsx
+++ src/pages/FrindDetalis.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router'
 import { RiAlarmWarningFill } from "react-icons/ri";
 import { FaArchive } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import Relation from '../Componats/Relation';
 
 
 const FrindDetalis = () => {
@@ -22,44 +23,71 @@ const FrindDetalis = () => {
   if(!frind)return<div>Loading...</div>
   
   return (
-    <div className='container mx-auto'>
-    <div className='shadow w-96 mt-3.5 '>
-    <div className="flex justify-center `">
-      <img
-      src={frind.picture}
-      className='rounded-full mt-2.5'
-      alt="Person"
-       />
-      
-    </div>
-    <h3 className='text-center font-bold'>{frind.name}</h3>
-      <div className=' flex justify-center items-center gap-2.5'>
-         {
-        frind.tags.map((tag,index)=>{
-        return <button className='btn  bg-green-500/50 text-[#244d3f] px-5 py-2 rounded-full ' key={index}>{tag}</button>
-        })
-      }
-      </div>
-    <div className='flex justify-center items-center mt-2.5 mb-2'>
-      <button className={`btn btn-sm w-32 mb-1.5 rounded-full  ${frind.status==="almost due"? 'btn-warning px-3':frind.status==="overdue"?'btn-error px-3':'btn-success px-3'}`} >{frind.status}</button>
+    <div className='container mx-auto grid grid-cols-2 gap-4'>
+                  <div>
+                    <div className='shadow w-full mt-3.5 '>
+                  <div className="flex justify-center `">
+                    <img
+                    src={frind.picture}
+                    className='rounded-full mt-2.5'
+                    alt="Person"
+                    />
+                    
+                  </div>
+                  <h3 className='text-center font-bold'>{frind.name}</h3>
+                    <div className=' flex justify-center items-center gap-2.5'>
+                      {
+                      frind.tags.map((tag,index)=>{
+                      return <button className='btn  bg-green-500/50 text-[#244d3f] px-5 py-2 rounded-full ' key={index}>{tag}</button>
+                      })
+                    }
+                    </div>
+                  <div className='flex justify-center items-center mt-2.5 mb-2'>
+                    <button className={`btn btn-sm w-32 mb-1.5 rounded-full  ${frind.status==="almost due"? 'btn-warning px-3':frind.status==="overdue"?'btn-error px-3':'btn-success px-3'}`} >{frind.status}</button>
 
-    </div>
+                  </div>
 
-    <p className="text-center text-sm mb-1">{frind.bio}</p>
-    <p className='text-center text-sm mb-3'>{frind.email}</p>
-      
-    </div>
+                  <p className="text-center text-sm mb-1">{frind.bio}</p>
+                  <p className='text-center text-sm mb-3'>{frind.email}</p>
+                    
+                  </div>
 
-    <div className='w-96 shadow text-center flex justify-center items-center gap-2 py-2.5'>
-    <RiAlarmWarningFill /> Snooze 2 weeks
-    </div>
-    <div className='w-96 shadow text-center flex justify-center items-center gap-2 py-2.5'>
-    <FaArchive /> Archive
-    </div>
-    <div className='w-96 shadow text-red-500 font-bold text-center flex justify-center items-center gap-2 py-2.5 mb-1.5'>
-    <MdDelete /> Delete
-    </div>
-    
+                  <div className='w-full shadow text-center flex justify-center items-center gap-2 py-2.5'>
+                  <RiAlarmWarningFill /> Snooze 2 weeks
+                  </div>
+                  <div className='w-full shadow text-center flex justify-center items-center gap-2 py-2.5'>
+                  <FaArchive /> Archive
+                  </div>
+                  <div className='w-full shadow text-red-500 font-bold text-center flex justify-center items-center gap-2 py-2.5 mb-1.5'>
+                  <MdDelete /> Delete
+                  </div>
+                  </div>
+                  <div>
+                    <div className='flex flex-row gap-4 '>
+                            <div className='w-full h-32 shadow  mt-3.5'>
+                                  <h1 className='text-center font-bold p-5 text-[#244d3f]'>{frind.days_since_contact}</h1>
+                                <p className='text-center'>days since contact</p>
+                            </div>  
+                            <div className='w-full h-32 shadow  mt-3.5'>
+                                  <h1 className='text-center font-bold p-5 text-[#244d3f]'>{frind.goal}</h1>
+                                <p className='text-center'>goal</p>
+                            </div>  
+                            <div className='w-full h-32 shadow  mt-3.5'>
+                                  <h1 className='text-center font-bold p-5 text-[#244d3f]'>{frind.next_due_date}</h1>
+                                <p className='text-center'>Next due</p>
+                            </div>  
+                  
+                  </div>
+                  <div className='mt-2.5'>
+                    <Relation />
+                  </div>
+                  </div>
+                  
+                  
+         <div>
+
+
+          </div>
     </div>
   )
 }
